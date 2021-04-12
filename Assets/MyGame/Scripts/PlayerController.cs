89d199e9f30eaf8f3d99e94b606af46009444ebd
Vichichi <aichhorn.vicky@gmail.com>
2021-04-12 22:06:53 +0200

    added magical strings

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@
 
 public class PlayerController : MonoBehaviour
 {
+    private const string JumpString = "Jump";
+    private const string GroundString = "Ground";
+    private const string ObstacleString = "Obstacle";
+    private const string SantaDeathString = "SantaDeath";
+
     Rigidbody2D rb;
     Animator anim, anim2, anim3, anim4, anim5;
     [SerializeField] float jumpForce;
@@ -32,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = Vector2.up * jumpForce;
 
-        anim.SetTrigger("Jump");
+        anim.SetTrigger(JumpString);
 
         GameManager.instance.IncrementScore();
     }
@@ -42,18 +47,21 @@ public class PlayerController : MonoBehaviour
         return true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)   {
-        if(collision.gameObject.tag == "Ground")
+    private void OnCollisionEnter2D(Collision2D collision)   
+    {
+        if(collision.gameObject.tag == GroundString)
         {
-            grounded = true;}
+            grounded = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle"){
+        if(collision.gameObject.tag == ObstacleString)
+        {
             GameManager.instance.GameOver();
             Destroy(collision.gameObject);
-            anim.Play("SantaDeath");
+            anim.Play(SantaDeathString);
             gameOver = SetGameOverTrue();
         }
     }
